da08014f7435bfbe195b121e497071df3fa66e71
Lini Mestar <lmEshoo@users.noreply.github.com>
2016-01-06 18:18:55 -0800

    Update controller.vhdl

--- controller.vhdl
+++ controller.vhdl
@@ -13,43 +13,50 @@ entity controller is
 end controller;
 
 architecture controllerA of controller is
+  signal counter: natural range 0 to 24;
   component LCDI is
-    Port( clk, Din, W, WADD, dataout, control );
+    Port( clk: IN std_logic;
+            DIN: OUT std_logic_vector( 8 downto 0 );
+            W: OUT std_logic;
+            WADD: OUT std_logic_vector( 5 downto 0 ); 
+            dataout: OUT std_logic_vector( 3 downto 0 ); 
+            control: OUT std_logic_vector( 2 downto 0 ));
   end component;
-  variable counter : integer := 0;
-
+  
   begin
+    display: LCDI port map(clk, DIN,W,WADD, dataout, control);
     process(CLK)
+      --variable counter : integer := 0;
       begin
         if(rising_edge(CLK)) then --CLK ='1' and CLK'event
           counter<= counter + 1;
           case counter is
-            WHEN 0 => W<=1;
-            WHEN 1 => W<=1; WADD<=5'b000000; Din=8'h49; --I
-            WHEN 2 => W<=1; WADD<=5'b000001; Din=8'h20; --
-            WHEN 3 => W<=1; WADD<=5'b000010; Din=8'h41; --A
-            WHEN 4 => W<=1; WADD<=5'b000011; Din=8'h4D; --M
-            WHEN 5 => W<=1; WADD<=5'b000100; Din=8'h20; --
-            WHEN 6 => W<=1; WADD<=5'b000101; Din=8'h2E; --.
-            WHEN 7 => W<=1; WADD<=5'b000110; Din=8'h2E; --.
-            WHEN 8 => W<=1; WADD<=5'b000111; Din=8'h2E; --.
-            WHEN 9 => W<=1; WADD<=5'b100000; Din=8'h20; --
-            WHEN 10 => W<=1; WADD<=5'b100001; Din=8'h20; --
-            WHEN 11 => W<=1; WADD<=5'b100010; Din=8'h20; --
-            WHEN 12 => W<=1; WADD<=5'b100011; Din=8'h20; --
-            WHEN 13 => W<=1; WADD<=5'b100100; Din=8'h20; --
-            WHEN 14 => W<=1; WADD<=5'b100101; Din=8'h20; --
-            WHEN 15 => W<=1; WADD<=5'b100110; Din=8'h20; --
-            WHEN 16 => W<=1; WADD<=5'b100111; Din=8'h20; --
-            WHEN 17 => W<=1; WADD<=5'b101000; Din=8'h57; --W
-            WHEN 18 => W<=1; WADD<=5'b101001; Din=8'h4F; --O
-            WHEN 19 => W<=1; WADD<=5'b101010; Din=8'h52; --R
-            WHEN 20 => W<=1; WADD<=5'b101011; Din=8'h4B; --K
-            WHEN 21 => W<=1; WADD<=5'b101100; Din=8'h49; --I
-            WHEN 22 => W<=1; WADD<=5'b101101; Din=8'h4E; --N
-            WHEN 23 => W<=1; WADD<=5'b101110; Din=8'h47; --G
+            WHEN 0 => W<='0';
+            WHEN 1 =>  W<='1'; WADD<="000000"; Din<=x"49";  --I
+            WHEN 2 => W<='1'; WADD<="000001"; Din<=x"20"; --
+            WHEN 3 => W<='1'; WADD<="000010"; Din<=x"41"; --A
+            WHEN 4 => W<='1'; WADD<="000011"; Din<=x"4D"; --M
+            WHEN 5 => W<='1'; WADD<="000100"; Din<=x"20"; --
+            WHEN 6 => W<='1'; WADD<="000101"; Din<=x"2E"; --.
+            WHEN 7 => W<='1'; WADD<="000110"; Din<=x"2E"; --.
+            WHEN 8 => W<='1'; WADD<="000111"; Din<=x"2E"; --.
+            WHEN 9 => W<='1'; WADD<="100000"; Din<=x"20"; --
+            WHEN 10 => W<='1'; WADD<="100001"; Din<=x"20"; --
+            WHEN 11 => W<='1'; WADD<="100010"; Din<=x"20"; --
+            WHEN 12 => W<='1'; WADD<="100011"; Din<=x"20"; --
+            WHEN 13 => W<='1'; WADD<="100100"; Din<=x"20"; --
+            WHEN 14 => W<='1'; WADD<="100101"; Din<=x"20"; --
+            WHEN 15 => W<='1'; WADD<="100110"; Din<=x"20"; --
+            WHEN 16 => W<='1'; WADD<="100111"; Din<=x"20"; --
+            WHEN 17 => W<='1'; WADD<="101000"; Din<=x"57"; --W
+            WHEN 18 => W<='1'; WADD<="101001"; Din<=x"4F"; --O
+            WHEN 19 => W<='1'; WADD<="101010"; Din<=x"52"; --R
+            WHEN 20 => W<='1'; WADD<="101011"; Din<=x"4B"; --K
+            WHEN 21 => W<='1'; WADD<="101100"; Din<=x"49"; --I
+            WHEN 22 => W<='1'; WADD<="101101"; Din<=x"4E"; --N
+            WHEN 23 => W<='1'; WADD<="101110"; Din<=x"47"; --G
 
-            WHEN 24 => W<=1;
+            WHEN 24 => W<='1';
           end case;
         end if;
   end process;
